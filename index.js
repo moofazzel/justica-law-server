32b4dfee641bef902691ff011d1ac4ebf71afdac
Md. Mofazzel Hossen <86169875+moofazzel@users.noreply.github.com>
2022-11-09 22:26:46 +0600

    mongodb added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 
 const port = process.env.PORT || 5000;
@@ -14,6 +16,16 @@ app.use(cors())
 app.get("/", function (req, res) {
     res.send("justica Server Running");
 });
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ia0tdiq.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
   
 app.listen(port, () =>
   console.log("Justica Server up and Running on port",port)
