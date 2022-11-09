1901bf6191c3eaa3f2605dbc97c07a8380680b20
Md. Mofazzel Hossen <86169875+moofazzel@users.noreply.github.com>
2022-11-10 00:12:53 +0600

    find all data done

--- index.js
+++ index.js
@@ -1,32 +1,66 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
-
 // Middle ware
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.get("/", function (req, res) {
-    res.send("justica Server Running");
+  res.send("justica Server Running");
 });
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ia0tdiq.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function connectDb() {
+  try {
+    await client.connect();
+    console.log("Database Connected");
+  } catch (error) {
+    console.log(error.name, error.massage, error.stack);
+  }
+}
 
+connectDb();
 
+const justicaDataCollections = client
+  .db("jastica-db")
+  .collection("justica-data");
 
-  
+// find all data
+try {
+  app.get("/data", async (req, res) => {
+    const query = {};
+    const cursor = justicaDataCollections.find(query);
+    const data = await cursor.toArray();
+    res.send(data);
+    console.log(data);
+  });
+} catch (error) {
+  console.log(error.name, error.massage, error.stack);
+}
+// find data with ID
+try {
+  app.get("/data/:id", async (req, res) => {
+    const { id } = req.params;
+    const query = { _id: ObjectId(id) };
+    const data = await justicaDataCollections.findOne(query);
+    res.send(data);
+  });
+} catch (error) {
+  console.log(error.name, error.massage, error.stack);
+}
+
 app.listen(port, () =>
-  console.log("Justica Server up and Running on port",port)
+  console.log("Justica Server up and Running on port", port)
 );
